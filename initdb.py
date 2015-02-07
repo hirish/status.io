1899809f3f7892f1ecf3fd9a1cf506e7a5335071
hirish <henry.irish@gmail.com>
2015-02-07 20:50:43 +0000

    Get friends on user model

--- initdb.py
+++ initdb.py
@@ -10,6 +10,13 @@ with app.app_context():
     neil  = User('neil')
     kira  = User('kira')
 
+    henry.friends = [james, adam]
+    james.friends = [kira, henry, adam]
+    adam.friends = [kira, neil]
+    neil.friends = [henry, adam]
+    kira.friends = [henry, james, adam, neil]
+    
+
     db.session.add(henry)
     db.session.add(james)
     db.session.add(adam)
--- models.py
+++ models.py
@@ -2,11 +2,21 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+friends = db.Table('friends',
+    db.Column('friend_1', db.Integer, db.ForeignKey('users.id')),
+    db.Column('friend_2', db.Integer, db.ForeignKey('users.id'))
+)
+
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+
+    friends = db.relationship('User',
+                            secondary=friends,
+                            primaryjoin = (friends.c.friend_1 == id),
+                            secondaryjoin = (friends.c.friend_2 == id))
 
     def __init__(self, name):
         self.name = name
@@ -15,4 +25,5 @@ class User(db.Model):
         return {
             'id' : self.id,
             'name' : self.name,
+            'friends': [friend.id for friend in self.friends]
         }
