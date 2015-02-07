30bcd4eec69dfda88e0196d0f964c41a994bace5
Neil Satra <github.k6p@gishpuppy.com>
2015-02-07 23:19:31 +0000

    Integrate basic classifier, and write results to database.

--- queueing.py
+++ queueing.py
@@ -1,11 +1,13 @@
 import requests
 from redis import Redis
 from rq import Queue
-from workerfunctions import count_words_at_url
+from workerfunctions import count_words_at_url, is_free
 import time
 
-q = Queue(connection=Redis())
-job = q.enqueue(count_words_at_url, 'http://nvie.com')
+r = Redis()
+q = Queue(connection=r)
+# job = q.enqueue(count_words_at_url, 'http://nvie.com')
+job = q.enqueue(is_free, 3, 0, 0, 0, 0, 0)
 
 print job.result
 time.sleep(10)
--- workerfunctions.py
+++ workerfunctions.py
@@ -1,6 +1,31 @@
 import requests
+from redis import Redis
 
 
 def count_words_at_url(url):
     resp = requests.get(url)
-    return len(resp.text.split())
+    wordcount = len(resp.text.split())
+    redisConnection = Redis()
+    redisConnection.set('wordcount', wordcount)
+
+
+def is_free(userid, location, accelerometer, ringer, call, calendar):
+    """
+        Input: location (0:home, 1:work, 2:other)
+        accelerometer (0:stationary, 1:mobile)
+        ringer (0:mute, 1:loud)
+        call (1: on call)
+        calendar (0:free, 0:busy)
+    """
+    status = 2
+
+    # Define when free (no acceleration, ringer on, not on call, nothing scheduled)
+    if accelerometer == 0 and ringer == 1 and call == 0 and calendar == 0:
+        status = 0
+    # Define when busy
+    if accelerometer == 1 or ringer == 0 or call == 1 or calendar == 1:
+        status = 1
+
+    Redis().set(userid, status)
+
+    return
